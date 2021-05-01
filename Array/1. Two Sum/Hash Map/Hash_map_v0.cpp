236f44c542a6cc57238145d9a0db3f46ee8ffea3
Philip-ZENG <72981101+Philip-ZENG@users.noreply.github.com>
2021-05-01 13:25:53 +0800

    Create Hash_map_v0.cpp

---
+++ Array/1. Two Sum/Hash Map/Hash_map_v0.cpp
@@ -0,0 +1,26 @@
+// Use hasp map instead of two for loop to improve seaching efficiency
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int size = nums.size();
+        unordered_map<int,int> hashtable;
+
+        for(int i = 0; i < size; i++){
+            int b = target - nums[i];
+            if(hashtable.find(b) != hashtable.end()){
+                return {hashtable.find(b)->second,i};
+            }else{
+                hashtable[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};

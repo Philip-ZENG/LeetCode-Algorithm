9f0ffcb780289a0365062810a20af0ed6428223f
Philip-ZENG <72981101+Philip-ZENG@users.noreply.github.com>
2021-05-03 12:09:10 +0800

    Create Two_pointers_v0.1.cpp

---
+++ Two Pointers/16. 3Sum Closest/Two Pointers/Two_pointers_v0.1.cpp
@@ -0,0 +1,52 @@
+/* Mild changes are made based on "Two_pointers_v0.cpp"
+ * Skip the repeated elements during each execution, which is the same as "15. 3Sum" question
+*/
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        long sum;
+        long closest = INT_MAX;
+        sort(nums.begin(),nums.end());
+
+        for(int i = 0; i < n; i++){
+            if(i == 0 || nums[i] != nums[i-1]){
+                for(int j = i+1; j < n; j++){
+                    int left = j;
+                    int right = n-1;
+                    while(left < right){
+                        sum = nums[i] + nums[left] + nums[right];
+                        if(abs(sum-target) < abs(closest-target)) closest = sum;
+                        if(sum == target) return closest;
+                        if(sum > target){
+                            while(left < right && nums[right] == nums[right-1]) right--;
+                            right--;
+                        }else{
+                            while(left < right && nums[left] == nums[left+1]) left++;
+                            left++;
+                        }
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+};
+
+int main() {
+    // Input argument
+    vector<int> nums = {-1,2,1,-4};
+    int target = 1;
+    // Test bench
+    Solution handle = Solution();
+    cout << handle.threeSumClosest(nums,target) << endl;
+    return 0;
+}

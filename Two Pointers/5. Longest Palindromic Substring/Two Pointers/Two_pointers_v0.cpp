e9142b88141b1952e9a252b5ce49152a14ebf83b
Philip-ZENG <72981101+Philip-ZENG@users.noreply.github.com>
2021-05-04 10:36:06 +0800

    Create Two_pointers_v0.cpp

---
+++ Two Pointers/5. Longest Palindromic Substring/Two Pointers/Two_pointers_v0.cpp
@@ -0,0 +1,68 @@
+/*
+ * First version of Two pointers method
+ * Use two pointers to slice substrings and check whether it is the longest palindrome
+ * Left pointer starts at index 0 and goes all the way to index n-1; (left pointer is fixed for each iteration)
+ * Right pointer starts at index n-1 and goes leftward: each time s[left] != s[right] decrease right by 1;
+ * Time complexity:O(n^2)
+ */
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.length() == 1) return s;
+        int left, right;;
+        int n = s.length();
+        string longest = "";
+        for(left = 0; left < n; left++){
+            right = n-1;
+            if(right-left+1 <= longest.length()) break;
+            while(left <= right){
+                if(s[left] == s[right]){
+                    string substring;
+                    if(left == right){
+                        substring = s[left];
+                    } else{
+                        substring = s.substr(left,right-left+1);
+                    }
+                    if(substring.length() > longest.length() && palindrome(substring)){
+                        longest = substring;
+                        break;
+                    }
+                }
+                right--;
+            }
+        }
+        return longest;
+    }
+
+    bool palindrome(string s){
+        if(s.length() == 1){
+            return true;
+        } else{
+            int left = 0;
+            int right = s.length()-1;
+            while(left <= right){
+                if(s[right] != s[left]) return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+};
+
+
+int main() {
+    // Input test data
+    string s = "babad";
+
+    // Test bench
+    Solution handle = Solution();
+    std::cout << handle.longestPalindrome(s) << std::endl;
+    return 0;
+}
